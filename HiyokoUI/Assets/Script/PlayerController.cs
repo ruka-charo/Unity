9bf3db2cbcedd48bb8409068f6d4bfb3cab44f8a
ruka charo <ruka.and.little.charo1@gmail.com>
2022-03-06 20:35:14 +0900

    ・ボタンの実行スクリプト

--- HiyokoUI/Assets/Script/PlayerController.cs
+++ HiyokoUI/Assets/Script/PlayerController.cs
@@ -14,13 +14,15 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // プレイヤーを動かす処理(入力に対してどっちに動くか)
-        transform.Translate(Input.GetAxisRaw(
-            "Horizontal") * speed * Time.deltaTime, 0, 0);
+        transform.Translate(
+            Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0, 0
+            );
 
         // 移動範囲を制限する処理(x軸のmin, maxの値を指定)
-        transform.position = new Vector2(Mathf.Clamp(
-            transform.position.x, -moveableRange, moveableRange),
-            transform.position.y);
+        transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x, -moveableRange, moveableRange),
+            transform.position.y
+            );
 
         // スペースキーを押したら砲弾が発射される
         if (Input.GetKeyDown(KeyCode.Space))
@@ -36,7 +38,6 @@ public class PlayerController : MonoBehaviour
         GameObject newBullet = Instantiate(cannonBall, spawnPoint.position,
                                Quaternion.identity) as GameObject;
         // 砲弾の飛んでいく処理(方向 × 強さ)
-        newBullet.GetComponent<Rigidbody2D>().AddForce(
-            Vector3.up * power);
+        newBullet.GetComponent<Rigidbody2D>().AddForce(Vector3.up * power);
     }
 }
